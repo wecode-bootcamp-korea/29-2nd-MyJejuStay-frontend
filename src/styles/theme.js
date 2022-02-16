9c451044f058482bce0a66b7cd3545a4a61bf377
Seokkitdo <sjd12329@gmail.com>
2022-02-16 13:57:53 +0900

    Modify : 색상 변수 설정

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,20 +1,9 @@
 const theme = {
-  white: '#fff',
-  mainColor: '#0A3442',
-  lightGray: '#D9D9D9',
-  middleGray: '#959595',
-  darkGray: '#666',
-  black: '#222',
-  highlight: '#5C7ECB',
-  small: '12px',
-  large: '18px',
-  big: '33px',
-  thin: '200',
-  thick: '800',
-  gray: '1px solid #D9D9D9',
-  borderMiddleGray: '1px solid #959595',
-  deepGray: '1px solid #666',
-  iconSmall: '16px',
+  color: '#717171',
+  starRed: '#FF385C',
+  starBlue: '#51ABF3',
+  border: '1px solid #dce4f0',
+  kakaoYellow: '#fce51e',
 };
 
 export default theme;

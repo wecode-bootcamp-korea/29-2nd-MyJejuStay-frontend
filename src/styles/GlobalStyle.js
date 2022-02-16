3a436eca50b72f0ba03380507317d7b00cb174cf
Seokkitdo <sjd12329@gmail.com>
2022-02-16 16:05:08 +0900

    Modify : globalStyle 수정

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -5,58 +5,7 @@ const GlobalStyle = createGlobalStyle`
   
   * {
     box-sizing: border-box;
-    padding: 0;
-    margin: 0;
   }
-  html {
-    width: 100%;
-    height: 100%;
-    font-size: 62.5%;
-  }
-  body {
-    width: 100%;
-    min-height: 100%;
-    font-family: 'SF Pro Display', sans-serif;
-    font-size: 14px;
-    line-height: 1.43;
-    color: #484848;
-    background-color: #fff;
-  }
-  a, a:hover, a:focus {
-    color: inherit;
-    text-decoration: none;
-    /* cursor: pointer; */
-  }
-  input,textarea {
-    outline: none;
-    border: 0;
-    background-color: transparent;
-  }
-  button {
-    background-color: transparent;
-    border: none;
-    outline: none;
-    cursor: pointer;
-  }
-  ol, ul, li {
-    list-style: none;
-  }
-  img {
-    width: 100%;
-  }
-  [tabindex]:focus-visible,
-  label:focus-visible,
-  button:focus-visible{
-    outline: none;
-    box-shadow: 0 0 0 3px rgba(0,0,0, 0.65);
-  }
-  .a11y {
-    overflow: hidden;
-    position: absolute;
-    border: 0;
-    width: 1px;
-    height: 1px;
-    clip: rect(1px, 1px, 1px, 1px);
-  }
+  
 `;
 export default GlobalStyle;

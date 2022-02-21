9873c00d8f40567ea7e690376979f249820fd0af
memoyoon <hazelmo0109@gmail.com>
2022-02-21 20:47:42 +0900

    Modify: 링크 변경

--- src/components/Nav/Theme.js
+++ src/components/Nav/Theme.js
@@ -1,5 +1,5 @@
 export const ThemeList = [
-  { id: 1, ThemeList: 'OCEAN', ThemeLink: "/Productlist?themaGroup='해변'" },
-  { id: 2, ThemeList: 'FOREST', ThemeLink: "/Productlist?themaGroup='숲'" },
-  { id: 3, ThemeList: 'CITY', ThemeLink: "/Productlist?themaGroup='도심'" },
+  { id: 1, ThemeList: 'OCEAN', ThemeLink: '/Productlist?themaGroup=해변' },
+  { id: 2, ThemeList: 'FOREST', ThemeLink: '/Productlist?themaGroup=숲' },
+  { id: 3, ThemeList: 'CITY', ThemeLink: '/Productlist?themaGroup=도심' },
 ];
